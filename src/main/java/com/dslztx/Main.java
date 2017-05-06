93372ccde270918bb15a0d1db0ba3d0f2d664556
dslztx <dslztx@gmail.com>
2017-05-06 20:07:26 +0800

    增加一个测试类

---
+++ src/main/java/com/dslztx/Main.java
@@ -0,0 +1,17 @@
+package com.dslztx;
+
+/**
+ * @author dslztx
+ */
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+        while (true) {
+            System.out.println("hello world " + (i++));
+            if (i == 1000000) {
+                break;
+            }
+            Thread.sleep(5000L);
+        }
+    }
+}

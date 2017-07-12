07c657716cb6741b73ef9103e7840580150884db
testuser <dslztx@gmail.com>
2017-07-12 21:36:17 +0800

    test

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author dslztx
+ * @date 2017年07月12日
+ */
+public class Main {
+    public static void main(String[] args) {
+        String a = "hello world";
+        System.out.println(a.substring(0, 0).length());
+    }
+}
--- src/main/java/com/dslztx/util/StringUtils.java
+++ src/main/java/com/dslztx/util/StringUtils.java
@@ -33,6 +33,8 @@ public class StringUtils {
             if (!CharUtil.isOctChar(s.charAt(index)))
                 return false;
         }
+
+
         return true;
     }
 

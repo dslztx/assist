07e26a9188bbf7a6383b9b278bb9b9b995088e68
dslztx <dslztx@gmail.com>
2017-10-20 14:04:36 +0800

    sync

--- src/main/java/me/dslztx/assist/algorithm/MD5.java
+++ src/main/java/me/dslztx/assist/algorithm/MD5.java
@@ -1,9 +1,18 @@
 package me.dslztx.assist.algorithm;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 /**
  * @author dslztx
  */
-//todo
 public class MD5 {
 
+  /**
+   * MD5算法
+   *
+   * @return 32位十六进制小写字符
+   */
+  public static String md5(String s) {
+    return DigestUtils.md5Hex(s);
+  }
 }

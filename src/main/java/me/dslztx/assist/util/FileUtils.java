df09fab14daa1150694d7287c3f696c853a1840a
dslztx <dslztx@gmail.com>
2017-11-03 17:40:38 +0800

    判断文件类型方法

--- src/main/java/me/dslztx/assist/util/FileUtils.java
+++ src/main/java/me/dslztx/assist/util/FileUtils.java
@@ -1,11 +1,24 @@
 package me.dslztx.assist.util;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author dslztx
  */
 public class FileUtils {
+
+  private static final Set<String> imgSuffixes = new HashSet<String>();
+
+  static {
+    imgSuffixes.add("gif");
+    imgSuffixes.add("jpg");
+    imgSuffixes.add("png");
+    imgSuffixes.add("jpeg");
+    imgSuffixes.add("bmp");
+    imgSuffixes.add("pic");
+  }
 
   public static boolean isDir(File file) {
     return file != null && file.isDirectory();
@@ -13,5 +26,38 @@ public class FileUtils {
 
   public static boolean isRegular(File file) {
     return file != null && file.isFile();
+  }
+
+  public static boolean isImage(File file) {
+    if (file == null) {
+      return false;
+    }
+
+    return isImage(file.getName());
+  }
+
+  public static boolean isImage(String name) {
+    if (StringUtils.isBlank(name)) {
+      return false;
+    }
+
+    String suffix = obtainSuffix(name);
+    if (StringUtils.isBlank(suffix)) {
+      return false;
+    }
+
+    return imgSuffixes.contains(suffix.toLowerCase());
+  }
+
+  public static String obtainSuffix(String name) {
+    if (StringUtils.isBlank(name)) {
+      return "";
+    }
+    int pos = name.lastIndexOf(".");
+    if (pos == -1 || pos == name.length() - 1) {
+      return "";
+    }
+
+    return name.substring(pos + 1);
   }
 }

ee9c9af9e0ebb76dc94d1ad1d2164eb0e0ac5473
dslztx <dslztx@gmail.com>
2017-11-28 16:22:33 +0800

    处理服务定义的受检异常与运行时异常问题

--- src/main/java/me/dslztx/assist/util/IOUtils.java
+++ src/main/java/me/dslztx/assist/util/IOUtils.java
@@ -24,56 +24,130 @@ public class IOUtils {
 
   private static final Logger logger = LoggerFactory.getLogger(IOUtils.class);
 
-  public static BufferedReader bufferedReader(File file, Charset charset)
-      throws FileNotFoundException {
-    return new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
+  public static BufferedReader bufferedReader(File file, Charset charset) {
+    if (file == null) {
+      throw new RuntimeException("file is null");
+    }
+    if (charset == null) {
+      throw new RuntimeException("charset is null");
+    }
+
+    try {
+      return new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public static BufferedReader bufferedReader(InputStream in, Charset charset) {
+    if (in == null) {
+      throw new RuntimeException("in is null");
+    }
+    if (charset == null) {
+      throw new RuntimeException("charset is null");
+    }
+
     return new BufferedReader(new InputStreamReader(in, charset));
   }
 
-  public static BufferedReader bufferedReader(File file) throws FileNotFoundException {
+  public static BufferedReader bufferedReader(File file) {
+    if (file == null) {
+      throw new RuntimeException("file is null");
+    }
+
     return bufferedReader(file, Charset.forName("UTF-8"));
   }
 
   public static BufferedReader bufferedReader(InputStream in) {
+    if (in == null) {
+      throw new RuntimeException("in is null");
+    }
+
     return bufferedReader(in, Charset.forName("UTF-8"));
   }
 
-  public static BufferedWriter bufferedWriter(File file, Charset charset)
-      throws FileNotFoundException {
-    return new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), charset));
+  public static BufferedWriter bufferedWriter(File file, Charset charset) {
+    if (file == null) {
+      throw new RuntimeException("file is null");
+    }
+    if (charset == null) {
+      throw new RuntimeException("charset is null");
+    }
+
+    try {
+      return new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), charset));
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public static BufferedWriter bufferedWriter(OutputStream out, Charset charset) {
+    if (out == null) {
+      throw new RuntimeException("out is null");
+    }
+    if (charset == null) {
+      throw new RuntimeException("charset is null");
+    }
+
     return new BufferedWriter(new OutputStreamWriter(out, charset));
   }
 
-  public static BufferedWriter bufferedWriter(File file) throws FileNotFoundException {
+  public static BufferedWriter bufferedWriter(File file) {
+    if (file == null) {
+      throw new RuntimeException("file is null");
+    }
+
     return bufferedWriter(file, Charset.forName("UTF-8"));
   }
 
   public static BufferedWriter bufferedWriter(OutputStream out) {
+    if (out == null) {
+      throw new RuntimeException("out is null");
+    }
+
     return bufferedWriter(out, Charset.forName("UTF-8"));
   }
 
-  public static BufferedInputStream bufferedInputStream(File file) throws FileNotFoundException {
-    return bufferedInputStream(new FileInputStream(file));
+  public static BufferedInputStream bufferedInputStream(File file) {
+    if (file == null) {
+      throw new RuntimeException("file is null");
+    }
+
+    try {
+      return bufferedInputStream(new FileInputStream(file));
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public static BufferedInputStream bufferedInputStream(InputStream in) {
+    if (in == null) {
+      throw new RuntimeException("in is null");
+    }
+
     if (in instanceof BufferedInputStream) {
       return (BufferedInputStream) in;
     }
     return new BufferedInputStream(in);
   }
 
-  public static BufferedOutputStream bufferedOutputStream(File file) throws FileNotFoundException {
-    return bufferedOutputStream(new FileOutputStream(file));
+  public static BufferedOutputStream bufferedOutputStream(File file) {
+    if (file == null) {
+      throw new RuntimeException("file is null");
+    }
+
+    try {
+      return bufferedOutputStream(new FileOutputStream(file));
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public static BufferedOutputStream bufferedOutputStream(OutputStream out) {
+    if (out == null) {
+      throw new RuntimeException("out is null");
+    }
+
     if (out instanceof BufferedOutputStream) {
       return (BufferedOutputStream) out;
     }

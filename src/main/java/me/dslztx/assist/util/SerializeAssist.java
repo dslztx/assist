c1c13f40c576cfc54189f112a4f96940880bf642
dslztx <dslztx@gmail.com>
2018-07-11 11:20:53 +0800

    增加SerializeAssist类和相应的测试

---
+++ src/main/java/me/dslztx/assist/util/SerializeAssist.java
@@ -0,0 +1,58 @@
+package me.dslztx.assist.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SerializeAssist {
+
+  private static final Logger logger = LoggerFactory.getLogger(SerializeAssist.class);
+
+  public static byte[] serializeToByteArray(Object obj) {
+    ByteArrayOutputStream bo = null;
+    ObjectOutputStream oo = null;
+    try {
+      bo = new ByteArrayOutputStream();
+
+      oo = new ObjectOutputStream(bo);
+      oo.writeObject(obj);
+      oo.flush();
+
+      return bo.toByteArray();
+    } catch (Exception e) {
+      logger.error("", e);
+      return null;
+    } finally {
+      if (oo != null) {
+        CloseableAssist.closeQuietly(oo);
+      }
+      if (bo != null) {
+        CloseableAssist.closeQuietly(bo);
+      }
+    }
+  }
+
+  public static <T> T deserializeFromByteArray(byte[] bytes, Class<T> clz) {
+    ByteArrayInputStream bi = null;
+    ObjectInputStream oi = null;
+    try {
+      bi = new ByteArrayInputStream(bytes);
+
+      oi = new ObjectInputStream(bi);
+      return clz.cast(oi.readObject());
+    } catch (Exception e) {
+      logger.error("", e);
+      return null;
+    } finally {
+      if (oi != null) {
+        CloseableAssist.closeQuietly(oi);
+      }
+      if (bi != null) {
+        CloseableAssist.closeQuietly(bi);
+      }
+    }
+  }
+}

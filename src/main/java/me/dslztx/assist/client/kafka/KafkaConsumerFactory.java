73199d56204f0cc43f8a495492d2e3095d39dbd9
dslztx <dslztx@gmail.com>
2018-07-11 10:10:33 +0800

    增加一个重载方法，参数为消费者组ID

--- src/main/java/me/dslztx/assist/client/kafka/KafkaConsumerFactory.java
+++ src/main/java/me/dslztx/assist/client/kafka/KafkaConsumerFactory.java
@@ -3,6 +3,7 @@ package me.dslztx.assist.client.kafka;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import me.dslztx.assist.util.StringAssist;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.lang3.StringUtils;
@@ -33,6 +34,37 @@ public class KafkaConsumerFactory {
 
       props.put("bootstrap.servers", servers);
       props.put("group.id", "defaultGroup");
+      props.put("enable.auto.commit", "true");
+      props.put("auto.commit.interval.ms", "1000");
+      props.put("session.timeout.ms", "30000");
+
+      return new KafkaConsumer<String, byte[]>(props, new StringDeserializer(),
+          new ByteArrayDeserializer());
+    } catch (Exception e) {
+      logger.error("", e);
+      return null;
+    }
+  }
+
+  public synchronized static KafkaConsumer<String, byte[]> obtainKafkaConsumer(String groupId) {
+    try {
+      if (StringAssist.isBlank(groupId)) {
+        throw new RuntimeException("groupId is null");
+      }
+
+      Configurations configs = new Configurations();
+      Configuration configuration = configs.properties(new File(CONFIG_FILE));
+
+      String servers = configuration.getString("kafka.servers");
+
+      if (StringUtils.isBlank(servers)) {
+        throw new RuntimeException("no kafka servers");
+      }
+
+      Map<String, Object> props = new HashMap<String, Object>();
+
+      props.put("bootstrap.servers", servers);
+      props.put("group.id", groupId);
       props.put("enable.auto.commit", "true");
       props.put("auto.commit.interval.ms", "1000");
       props.put("session.timeout.ms", "30000");

3d9af9baa6e78c2522e85efd713ef5ce9fe18928
dslztx <dslztx@gmail.com>
2018-08-20 01:24:47 +0800

    增加UUIDAssist类和相应的测试类

---
+++ src/main/java/me/dslztx/assist/util/UUIDAssist.java
@@ -0,0 +1,14 @@
+package me.dslztx.assist.util;
+
+import java.util.UUID;
+
+/**
+ * @author dslztx
+ */
+public class UUIDAssist {
+
+  public static String obtainUUID() {
+    return UUID.randomUUID().toString();
+  }
+
+}
---
+++ src/test/java/me/dslztx/assist/util/UUIDAssistTest.java
@@ -0,0 +1,11 @@
+package me.dslztx.assist.util;
+
+import org.junit.Test;
+
+public class UUIDAssistTest {
+
+  @Test
+  public void obtainUUID() {
+    System.out.println(UUIDAssist.obtainUUID());
+  }
+}

c561b875c46f15326f570ea0da677876e777059e
dslztx <dslztx@gmail.com>
2018-08-20 20:34:30 +0800

    Dao类及其子类方法中避免引入非线程安全语句

--- src/main/java/me/dslztx/assist/dao/Dao.java
+++ src/main/java/me/dslztx/assist/dao/Dao.java
@@ -2,6 +2,9 @@ package me.dslztx.assist.dao;
 
 import javax.sql.DataSource;
 
+/**
+ * Dao类及其子类应用场景为：单实例，多线程。因此，须避免在方法中引入非线程安全语句
+ */
 public abstract class Dao {
 
   private DataSource dataSource;

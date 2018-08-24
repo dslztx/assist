d5d6f797f9c8838971c8c7490d0734d7560ae6e4
dslztx <dslztx@gmail.com>
2018-08-24 14:13:18 +0800

    遵循Alibaba规范重新格式化

--- src/main/java/me/dslztx/assist/client/redis/RedisClient.java
+++ src/main/java/me/dslztx/assist/client/redis/RedisClient.java
@@ -1,96 +1,98 @@
 package me.dslztx.assist.client.redis;
 
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 public class RedisClient {
 
-  private static final Logger logger = LoggerFactory.getLogger(RedisClient.class);
+    private static final Logger logger = LoggerFactory.getLogger(RedisClient.class);
 
-  public Jedis jedis;
+    public Jedis jedis;
 
-  private JedisPoolProxy jedisPoolProxy;
+    private JedisPoolProxy jedisPoolProxy;
 
-  public RedisClient(Jedis jedis, JedisPoolProxy jedisPoolProxy) {
-    this.jedis = jedis;
-    this.jedisPoolProxy = jedisPoolProxy;
-  }
-
-  public void returnResource() {
-    try {
-      if (jedis != null && jedisPoolProxy != null) {
-        jedisPoolProxy.getPool().returnResource(jedis);
-      }
-    } catch (Exception e) {
-      logger.error("", e);
-
-      if (jedis != null && jedisPoolProxy != null) {
-        jedisPoolProxy.getPool().returnBrokenResource(jedis);
-      }
-    } finally {
-      jedis = null;
-      jedisPoolProxy = null;
+    public RedisClient(Jedis jedis, JedisPoolProxy jedisPoolProxy) {
+        this.jedis = jedis;
+        this.jedisPoolProxy = jedisPoolProxy;
     }
-  }
+
+    public void returnResource() {
+        try {
+            if (jedis != null && jedisPoolProxy != null) {
+                jedisPoolProxy.getPool().returnResource(jedis);
+            }
+        } catch (Exception e) {
+            logger.error("", e);
+
+            if (jedis != null && jedisPoolProxy != null) {
+                jedisPoolProxy.getPool().returnBrokenResource(jedis);
+            }
+        } finally {
+            jedis = null;
+            jedisPoolProxy = null;
+        }
+    }
 }
 
 class JedisPoolProxy {
 
-  private static final Logger logger = LoggerFactory.getLogger(JedisPoolProxy.class);
+    private static final Logger logger = LoggerFactory.getLogger(JedisPoolProxy.class);
 
-  //冻结10秒
-  private static final long BLOCKED_TIME = 10 * 1000;
+    // 冻结10秒
+    private static final long BLOCKED_TIME = 10 * 1000;
 
-  String server;
+    String server;
 
-  JedisPool pool;
+    JedisPool pool;
 
-  AtomicLong blockedTime = new AtomicLong(-1);
+    AtomicLong blockedTime = new AtomicLong(-1);
 
-  public JedisPoolProxy(String server, JedisPool pool) {
-    this.server = server;
-    this.pool = pool;
-  }
-
-  public boolean isBlocked() {
-    if (blockedTime.get() < 0) {
-      return false;
+    public JedisPoolProxy(String server, JedisPool pool) {
+        this.server = server;
+        this.pool = pool;
     }
 
-    if (System.currentTimeMillis() - blockedTime.get() > BLOCKED_TIME) {
-      logger.info("server " + server + " recovers");
-      blockedTime.set(-1);
-      return false;
+    public boolean isBlocked() {
+        if (blockedTime.get() < 0) {
+            return false;
+        }
+
+        if (System.currentTimeMillis() - blockedTime.get() > BLOCKED_TIME) {
+            logger.info("server " + server + " recovers");
+            blockedTime.set(-1);
+            return false;
+        }
+
+        return true;
     }
 
-    return true;
-  }
+    public String getServer() {
+        return server;
+    }
 
-  public String getServer() {
-    return server;
-  }
+    public void setServer(String server) {
+        this.server = server;
+    }
 
-  public void setServer(String server) {
-    this.server = server;
-  }
+    public JedisPool getPool() {
+        return pool;
+    }
 
-  public JedisPool getPool() {
-    return pool;
-  }
+    public void setPool(JedisPool pool) {
+        this.pool = pool;
+    }
 
-  public void setPool(JedisPool pool) {
-    this.pool = pool;
-  }
+    public long getBlockedTime() {
+        return blockedTime.get();
+    }
 
-  public long getBlockedTime() {
-    return blockedTime.get();
-  }
-
-  public void setBlockedTime(long blockedTime) {
-    logger.info("server " + server + " is blocked");
-    this.blockedTime.set(blockedTime);
-  }
+    public void setBlockedTime(long blockedTime) {
+        logger.info("server " + server + " is blocked");
+        this.blockedTime.set(blockedTime);
+    }
 }
--- src/main/java/me/dslztx/assist/client/redis/RedisClientFactory.java
+++ src/main/java/me/dslztx/assist/client/redis/RedisClientFactory.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import me.dslztx.assist.util.StringAssist;
+
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import me.dslztx.assist.util.StringAssist;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
@@ -17,139 +19,137 @@ import redis.clients.jedis.JedisPoolConfig;
 @SuppressWarnings("Duplicates")
 public class RedisClientFactory {
 
-  private static final Logger logger = LoggerFactory.getLogger(RedisClientFactory.class);
+    private static final Logger logger = LoggerFactory.getLogger(RedisClientFactory.class);
 
-  private static final int MAX_TOTAL = 30;
-  private static final int MAX_IDLE = 10;
-  private static final int MAX_WAIT_MILLIS = 1000;
-  private static final boolean TEST_ON_BORROW = false;
-  private static final boolean TEST_WHILE_IDLE = false;
-  private static final int TIMEOUT = 5000;
+    private static final int MAX_TOTAL = 30;
+    private static final int MAX_IDLE = 10;
+    private static final int MAX_WAIT_MILLIS = 1000;
+    private static final boolean TEST_ON_BORROW = false;
+    private static final boolean TEST_WHILE_IDLE = false;
+    private static final int TIMEOUT = 5000;
 
+    private static final String CONFIG_FILE = "redis.properties";
 
-  private static final String CONFIG_FILE = "redis.properties";
+    private static volatile boolean init = false;
 
-  private static volatile boolean init = false;
+    private static Map<String, List<JedisPoolProxy>> groupedJedisPools = new HashMap<String, List<JedisPoolProxy>>();
 
-  private static Map<String, List<JedisPoolProxy>> groupedJedisPools = new HashMap<String, List<JedisPoolProxy>>();
-
-  public static RedisClient obtainRedisClient(String group) {
-    if (!init) {
-      init();
-    }
-
-    if (groupedJedisPools.get(group) == null) {
-      return null;
-    }
-
-    return obtainRedisClient(groupedJedisPools.get(group));
-  }
-
-  private static RedisClient obtainRedisClient(List<JedisPoolProxy> jedisPools) {
-    int idx = (int) (Math.random() * jedisPools.size());
-
-    for (int index = idx; index < jedisPools.size(); index++) {
-      if (!jedisPools.get(index).isBlocked()) {
-        try {
-          Jedis x = jedisPools.get(index).getPool().getResource();
-
-          if (x != null) {
-            return new RedisClient(x, jedisPools.get(index));
-          }
-        } catch (Throwable e) {
-          logger.error("", e);
-          jedisPools.get(index).setBlockedTime(System.currentTimeMillis());
-        }
-      }
-    }
-
-    for (int index = 0; index < idx; index++) {
-      if (!jedisPools.get(index).isBlocked()) {
-        try {
-          Jedis x = jedisPools.get(index).getPool().getResource();
-
-          if (x != null) {
-            return new RedisClient(x, jedisPools.get(index));
-          }
-        } catch (Throwable e) {
-          logger.error("", e);
-          jedisPools.get(index).setBlockedTime(System.currentTimeMillis());
-        }
-      }
-    }
-
-    return null;
-  }
-
-  private static void init() {
-    if (!init) {
-      synchronized (RedisClientFactory.class) {
+    public static RedisClient obtainRedisClient(String group) {
         if (!init) {
-          try {
-            Configurations configs = new Configurations();
-
-            Configuration configuration = configs.properties(new File(CONFIG_FILE));
-
-            String groups = configuration.getString("groups");
-            if (StringAssist.isBlank(groups)) {
-              throw new RuntimeException("no groups");
-            }
-
-            String[] groupArray = groups.split(",");
-            for (String group : groupArray) {
-              logger.info("generate jedispools for group: " + group);
-              Configuration subConfiguration = configuration.subset(group);
-              groupedJedisPools.put(group, generateJedisPools(subConfiguration));
-            }
-          } catch (Exception e) {
-            logger.error("", e);
-            throw new RuntimeException(e);
-          } finally {
-            init = true;
-          }
+            init();
         }
-      }
-    }
-  }
 
-  private static List<JedisPoolProxy> generateJedisPools(Configuration configuration) {
-    List<JedisPoolProxy> jedisPools = new ArrayList<JedisPoolProxy>();
+        if (groupedJedisPools.get(group) == null) {
+            return null;
+        }
 
-    if (configuration == null) {
-      return jedisPools;
+        return obtainRedisClient(groupedJedisPools.get(group));
     }
 
-    String servers = configuration.getString("redis.servers");
-    if (StringAssist.isBlank(servers)) {
-      logger.error("no redis servers");
-      return jedisPools;
+    private static RedisClient obtainRedisClient(List<JedisPoolProxy> jedisPools) {
+        int idx = (int)(Math.random() * jedisPools.size());
+
+        for (int index = idx; index < jedisPools.size(); index++) {
+            if (!jedisPools.get(index).isBlocked()) {
+                try {
+                    Jedis x = jedisPools.get(index).getPool().getResource();
+
+                    if (x != null) {
+                        return new RedisClient(x, jedisPools.get(index));
+                    }
+                } catch (Throwable e) {
+                    logger.error("", e);
+                    jedisPools.get(index).setBlockedTime(System.currentTimeMillis());
+                }
+            }
+        }
+
+        for (int index = 0; index < idx; index++) {
+            if (!jedisPools.get(index).isBlocked()) {
+                try {
+                    Jedis x = jedisPools.get(index).getPool().getResource();
+
+                    if (x != null) {
+                        return new RedisClient(x, jedisPools.get(index));
+                    }
+                } catch (Throwable e) {
+                    logger.error("", e);
+                    jedisPools.get(index).setBlockedTime(System.currentTimeMillis());
+                }
+            }
+        }
+
+        return null;
     }
 
-    String[] serverArray = servers.split(",");
+    private static void init() {
+        if (!init) {
+            synchronized (RedisClientFactory.class) {
+                if (!init) {
+                    try {
+                        Configurations configs = new Configurations();
 
-    JedisPoolConfig config = new JedisPoolConfig();
-    config.setMaxTotal(MAX_TOTAL);
-    config.setMaxIdle(MAX_IDLE);
-    config.setMaxWaitMillis(MAX_WAIT_MILLIS);
-    config.setTestOnBorrow(TEST_ON_BORROW);
-    config.setTestWhileIdle(TEST_WHILE_IDLE);
+                        Configuration configuration = configs.properties(new File(CONFIG_FILE));
 
-    for (String server : serverArray) {
-      try {
-        String host = server.split(":")[0];
-        int port = Integer.parseInt(server.split(":")[1]);
+                        String groups = configuration.getString("groups");
+                        if (StringAssist.isBlank(groups)) {
+                            throw new RuntimeException("no groups");
+                        }
 
-        JedisPoolProxy proxy = new JedisPoolProxy(server,
-            new JedisPool(config, host, port, TIMEOUT));
-
-        jedisPools.add(proxy);
-      } catch (Exception e) {
-        logger.error(server, e);
-      }
+                        String[] groupArray = groups.split(",");
+                        for (String group : groupArray) {
+                            logger.info("generate jedispools for group: " + group);
+                            Configuration subConfiguration = configuration.subset(group);
+                            groupedJedisPools.put(group, generateJedisPools(subConfiguration));
+                        }
+                    } catch (Exception e) {
+                        logger.error("", e);
+                        throw new RuntimeException(e);
+                    } finally {
+                        init = true;
+                    }
+                }
+            }
+        }
     }
 
-    logger.info("jedisPools size : " + jedisPools.size());
+    private static List<JedisPoolProxy> generateJedisPools(Configuration configuration) {
+        List<JedisPoolProxy> jedisPools = new ArrayList<JedisPoolProxy>();
 
-    return jedisPools;
-  }
+        if (configuration == null) {
+            return jedisPools;
+        }
+
+        String servers = configuration.getString("redis.servers");
+        if (StringAssist.isBlank(servers)) {
+            logger.error("no redis servers");
+            return jedisPools;
+        }
+
+        String[] serverArray = servers.split(",");
+
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(MAX_TOTAL);
+        config.setMaxIdle(MAX_IDLE);
+        config.setMaxWaitMillis(MAX_WAIT_MILLIS);
+        config.setTestOnBorrow(TEST_ON_BORROW);
+        config.setTestWhileIdle(TEST_WHILE_IDLE);
+
+        for (String server : serverArray) {
+            try {
+                String host = server.split(":")[0];
+                int port = Integer.parseInt(server.split(":")[1]);
+
+                JedisPoolProxy proxy = new JedisPoolProxy(server, new JedisPool(config, host, port, TIMEOUT));
+
+                jedisPools.add(proxy);
+            } catch (Exception e) {
+                logger.error(server, e);
+            }
+        }
+
+        logger.info("jedisPools size : " + jedisPools.size());
+
+        return jedisPools;
+    }
 }

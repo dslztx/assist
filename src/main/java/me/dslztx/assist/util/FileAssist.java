dbcf0256ad9ce794636b066836fe304a7a8e35cf
dslztx <dslztx@gmail.com>
2018-08-30 02:37:10 +0800

    增加一个获取类路径下文件的方法

--- src/main/java/me/dslztx/assist/util/FileAssist.java
+++ src/main/java/me/dslztx/assist/util/FileAssist.java
@@ -4,11 +4,14 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,245 +21,261 @@ import org.slf4j.LoggerFactory;
  */
 public class FileAssist {
 
-  private static final Logger logger = LoggerFactory.getLogger(FileAssist.class);
+    private static final Logger logger = LoggerFactory.getLogger(FileAssist.class);
 
-  private static final Set<String> imgSuffixes = new HashSet<String>();
+    private static final Set<String> imgSuffixes = new HashSet<String>();
 
-  private static final AtomicLong uniqueID = new AtomicLong(System.currentTimeMillis());
+    private static final AtomicLong uniqueID = new AtomicLong(System.currentTimeMillis());
 
-  static {
-    imgSuffixes.add("gif");
-    imgSuffixes.add("jpg");
-    imgSuffixes.add("png");
-    imgSuffixes.add("jpeg");
-    imgSuffixes.add("bmp");
-    imgSuffixes.add("pic");
-  }
-
-  public static void createSubDirInDir(String parentDir, String subDir) {
-    try {
-      FileUtils.forceMkdir(new File(parentDir + File.separator + subDir));
-    } catch (Exception e) {
-      logger.error("", e);
-      throw new RuntimeException(e);
-    }
-  }
-
-  public static File fileInDir(String parentDir, String file) {
-    return new File(parentDir + File.separator + file);
-  }
-
-  public static void copyClassPathFileToLocalDir(String name, boolean overwrite) {
-    File dst = new File(name);
-
-    if (!overwrite) {
-      if (dst.exists()) {
-        return;
-      }
+    static {
+        imgSuffixes.add("gif");
+        imgSuffixes.add("jpg");
+        imgSuffixes.add("png");
+        imgSuffixes.add("jpeg");
+        imgSuffixes.add("bmp");
+        imgSuffixes.add("pic");
     }
 
-    InputStream in = null;
-    try {
-      in = ClassLoader.getSystemClassLoader().getResourceAsStream(name);
-      FileUtils.copyInputStreamToFile(in, dst);
-    } catch (Throwable e) {
-      logger.error("", e);
-    }
-  }
-
-  public static void copyClassPathFileToDst(String name, String destination, boolean overwrite) {
-    File dst = new File(destination);
-
-    if (!overwrite) {
-      if (dst.exists()) {
-        return;
-      }
-    }
-
-    InputStream in = null;
-    try {
-      in = ClassLoader.getSystemClassLoader().getResourceAsStream(name);
-      FileUtils.copyInputStreamToFile(in, dst);
-    } catch (Throwable e) {
-      logger.error("", e);
-    }
-  }
-
-  public static void delFileRecursiveForce(File a) {
-    if (a == null || !a.exists()) {
-      return;
-    }
-
-    if (a.isFile()) {
-      a.delete();
-    } else {
-      if (ArrayAssist.isEmpty(a.listFiles())) {
-        a.delete();
-      } else {
-        for (File file : a.listFiles()) {
-          if (file.isFile()) {
-            file.delete();
-          } else {
-            delFileRecursiveForce(file);
-          }
+    public static void createSubDirInDir(String parentDir, String subDir) {
+        try {
+            FileUtils.forceMkdir(new File(parentDir + File.separator + subDir));
+        } catch (Exception e) {
+            logger.error("", e);
+            throw new RuntimeException(e);
         }
-        a.delete();
-      }
-    }
-  }
-
-  public static boolean isDirSame(File a, File b) {
-    if (!FileAssist.isDir(a) || !FileAssist.isDir(b)) {
-      throw new RuntimeException("存在非法目录");
     }
 
-    try {
-      List<String> aFileNames = new ArrayList<String>();
-      List<String> bFileNames = new ArrayList<String>();
-      if (a.listFiles() != null) {
-        for (File file : a.listFiles()) {
-          aFileNames.add(file.getName());
+    public static File fileInDir(String parentDir, String file) {
+        return new File(parentDir + File.separator + file);
+    }
+
+    public static void copyClassPathFileToLocalDir(String name, boolean overwrite) {
+        File dst = new File(name);
+
+        if (!overwrite) {
+            if (dst.exists()) {
+                return;
+            }
         }
-      }
 
-      if (b.listFiles() != null) {
-        for (File file : b.listFiles()) {
-          bFileNames.add(file.getName());
+        InputStream in = null;
+        try {
+            in = ClassLoader.getSystemClassLoader().getResourceAsStream(name);
+            FileUtils.copyInputStreamToFile(in, dst);
+        } catch (Throwable e) {
+            logger.error("", e);
         }
-      }
+    }
 
-      if (!aFileNames.equals(bFileNames)) {
-        return false;
-      }
+    public static void copyClassPathFileToDst(String name, String destination, boolean overwrite) {
+        File dst = new File(destination);
 
-      File aSubFile = null;
-      File bSubFile = null;
-      for (String fileName : aFileNames) {
-        aSubFile = new File(a.getCanonicalPath() + File.separator + fileName);
-        bSubFile = new File(b.getCanonicalPath() + File.separator + fileName);
-        if (aSubFile.isFile() && bSubFile.isFile()) {
-          if (!isContentSame(aSubFile, bSubFile)) {
-            return false;
-          }
-        } else if (aSubFile.isDirectory() && bSubFile.isDirectory()) {
-          if (!isDirSame(aSubFile, bSubFile)) {
-            return false;
-          }
+        if (!overwrite) {
+            if (dst.exists()) {
+                return;
+            }
+        }
+
+        InputStream in = null;
+        try {
+            in = ClassLoader.getSystemClassLoader().getResourceAsStream(name);
+            FileUtils.copyInputStreamToFile(in, dst);
+        } catch (Throwable e) {
+            logger.error("", e);
+        }
+    }
+
+    public static void delFileRecursiveForce(File a) {
+        if (a == null || !a.exists()) {
+            return;
+        }
+
+        if (a.isFile()) {
+            a.delete();
         } else {
-          return false;
+            if (ArrayAssist.isEmpty(a.listFiles())) {
+                a.delete();
+            } else {
+                for (File file : a.listFiles()) {
+                    if (file.isFile()) {
+                        file.delete();
+                    } else {
+                        delFileRecursiveForce(file);
+                    }
+                }
+                a.delete();
+            }
         }
-      }
-      return true;
-    } catch (Exception e) {
-      logger.error("", e);
-      return false;
-    }
-  }
-
-  public static boolean isContentSame(File a, File b) {
-    if (a == null || b == null) {
-      return false;
     }
 
-    BufferedInputStream ain = null;
-    BufferedInputStream bin = null;
-    try {
-      ain = IOAssist.bufferedInputStream(a);
-      bin = IOAssist.bufferedInputStream(b);
-
-      int av = 0;
-      int bv = 0;
-      while (true) {
-        av = ain.read();
-        bv = bin.read();
-        if (av == -1 || bv == -1) {
-          break;
+    public static boolean isDirSame(File a, File b) {
+        if (!FileAssist.isDir(a) || !FileAssist.isDir(b)) {
+            throw new RuntimeException("存在非法目录");
         }
 
-        if (av != bv) {
-          return false;
+        try {
+            List<String> aFileNames = new ArrayList<String>();
+            List<String> bFileNames = new ArrayList<String>();
+            if (a.listFiles() != null) {
+                for (File file : a.listFiles()) {
+                    aFileNames.add(file.getName());
+                }
+            }
+
+            if (b.listFiles() != null) {
+                for (File file : b.listFiles()) {
+                    bFileNames.add(file.getName());
+                }
+            }
+
+            if (!aFileNames.equals(bFileNames)) {
+                return false;
+            }
+
+            File aSubFile = null;
+            File bSubFile = null;
+            for (String fileName : aFileNames) {
+                aSubFile = new File(a.getCanonicalPath() + File.separator + fileName);
+                bSubFile = new File(b.getCanonicalPath() + File.separator + fileName);
+                if (aSubFile.isFile() && bSubFile.isFile()) {
+                    if (!isContentSame(aSubFile, bSubFile)) {
+                        return false;
+                    }
+                } else if (aSubFile.isDirectory() && bSubFile.isDirectory()) {
+                    if (!isDirSame(aSubFile, bSubFile)) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        } catch (Exception e) {
+            logger.error("", e);
+            return false;
         }
-      }
-
-      if (av == -1 && bv == -1) {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (IOException e) {
-      logger.error("", e);
-      return false;
-    } finally {
-      CloseableAssist.close(ain);
-      CloseableAssist.close(bin);
-    }
-  }
-
-  public static boolean isDir(File file) {
-    return file != null && file.isDirectory();
-  }
-
-  public static boolean isRegular(File file) {
-    return file != null && file.isFile();
-  }
-
-  public static boolean isImage(File file) {
-    if (file == null) {
-      return false;
     }
 
-    return isImage(file.getName());
-  }
+    public static boolean isContentSame(File a, File b) {
+        if (a == null || b == null) {
+            return false;
+        }
 
-  public static boolean isImage(String name) {
-    if (StringAssist.isBlank(name)) {
-      return false;
+        BufferedInputStream ain = null;
+        BufferedInputStream bin = null;
+        try {
+            ain = IOAssist.bufferedInputStream(a);
+            bin = IOAssist.bufferedInputStream(b);
+
+            int av = 0;
+            int bv = 0;
+            while (true) {
+                av = ain.read();
+                bv = bin.read();
+                if (av == -1 || bv == -1) {
+                    break;
+                }
+
+                if (av != bv) {
+                    return false;
+                }
+            }
+
+            if (av == -1 && bv == -1) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (IOException e) {
+            logger.error("", e);
+            return false;
+        } finally {
+            CloseableAssist.close(ain);
+            CloseableAssist.close(bin);
+        }
     }
 
-    String suffix = obtainSuffix(name);
-    if (StringAssist.isBlank(suffix)) {
-      return false;
+    public static boolean isDir(File file) {
+        return file != null && file.isDirectory();
     }
 
-    return imgSuffixes.contains(suffix.toLowerCase());
-  }
-
-  public static String obtainSuffix(String name) {
-    if (StringAssist.isBlank(name)) {
-      return "";
-    }
-    int pos = name.lastIndexOf(".");
-    if (pos == -1 || pos == name.length() - 1) {
-      return "";
+    public static boolean isRegular(File file) {
+        return file != null && file.isFile();
     }
 
-    return name.substring(pos + 1);
-  }
+    public static boolean isImage(File file) {
+        if (file == null) {
+            return false;
+        }
 
-  public static void createUniqueFile(File dir, String suffix) throws IOException {
-    if (!isDir(dir)) {
-      throw new RuntimeException("dir is not a legal directory");
+        return isImage(file.getName());
     }
 
-    long t = System.currentTimeMillis();
+    public static boolean isImage(String name) {
+        if (StringAssist.isBlank(name)) {
+            return false;
+        }
 
-    File file = new File(
-        (dir.getCanonicalPath() + File.separator + (t++) + (StringAssist.isBlank(suffix) ? ""
-            : suffix)));
-    while (file.exists()) {
-      file = new File(
-          dir.getCanonicalPath() + File.separator + (t++) + (StringAssist.isBlank(suffix) ? ""
-              : suffix));
+        String suffix = obtainSuffix(name);
+        if (StringAssist.isBlank(suffix)) {
+            return false;
+        }
+
+        return imgSuffixes.contains(suffix.toLowerCase());
     }
 
-    try {
-      file.createNewFile();
-    } catch (IOException e) {
-      throw new RuntimeException("create file [" + file.getCanonicalPath() + "] fail");
-    }
-  }
+    public static String obtainSuffix(String name) {
+        if (StringAssist.isBlank(name)) {
+            return "";
+        }
+        int pos = name.lastIndexOf(".");
+        if (pos == -1 || pos == name.length() - 1) {
+            return "";
+        }
 
-  public static long obtainApplicationUniqueID() {
-    return uniqueID.getAndIncrement();
-  }
+        return name.substring(pos + 1);
+    }
+
+    public static void createUniqueFile(File dir, String suffix) throws IOException {
+        if (!isDir(dir)) {
+            throw new RuntimeException("dir is not a legal directory");
+        }
+
+        long t = System.currentTimeMillis();
+
+        File file =
+            new File((dir.getCanonicalPath() + File.separator + (t++) + (StringAssist.isBlank(suffix) ? "" : suffix)));
+        while (file.exists()) {
+            file = new File(
+                dir.getCanonicalPath() + File.separator + (t++) + (StringAssist.isBlank(suffix) ? "" : suffix));
+        }
+
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("create file [" + file.getCanonicalPath() + "] fail");
+        }
+    }
+
+    public static long obtainApplicationUniqueID() {
+        return uniqueID.getAndIncrement();
+    }
+
+    public static File[] locateClassPathFiles(String name) {
+        List<File> files = new ArrayList<File>();
+
+        try {
+            Enumeration<URL> urls = ClassLoader.getSystemClassLoader().getResources(name);
+            if (urls != null) {
+                while (urls.hasMoreElements()) {
+                    URL url = urls.nextElement();
+                    files.add(new File(url.getFile()));
+                }
+            }
+        } catch (IOException e) {
+            logger.error("", e);
+        }
+
+        return files.toArray(new File[files.size()]);
+    }
 }

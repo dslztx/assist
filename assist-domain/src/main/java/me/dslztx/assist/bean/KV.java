dc2cb4976879a27a1e507351c94ab879ac7c02f4
dslztx <dslztx@gmail.com>
2018-10-25 03:43:02 +0800

    增加KV类表示键值对

---
+++ assist-domain/src/main/java/me/dslztx/assist/bean/KV.java
@@ -0,0 +1,23 @@
+package me.dslztx.assist.bean;
+
+public class KV {
+    private String key;
+
+    private Object value;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+}

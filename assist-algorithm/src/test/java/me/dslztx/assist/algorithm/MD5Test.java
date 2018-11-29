b751bf8ba55182017c8166af45a91b974c1be2aa
dslztx <dslztx@gmail.com>
2018-11-29 21:01:10 +0800

    增加测试用例

--- assist-algorithm/src/test/java/me/dslztx/assist/algorithm/MD5Test.java
+++ assist-algorithm/src/test/java/me/dslztx/assist/algorithm/MD5Test.java
@@ -7,14 +7,19 @@ import org.junit.Test;
 
 public class MD5Test {
 
-  @Test
-  public void md5() throws Exception {
-    try {
-      assertTrue(MD5.md5("hello world").equals("5eb63bbbe01eeed093cb22bb8f5acdc3"));
-      assertTrue(MD5.md5("great wall").equals("1bf62cc6bd5ae248f6ee26d395502ecc"));
-    } catch (Exception e) {
-      fail();
+    @Test
+    public void md5() throws Exception {
+        try {
+            assertTrue(MD5.md5("hello world").equals("5eb63bbbe01eeed093cb22bb8f5acdc3"));
+            assertTrue(MD5.md5("great wall").equals("1bf62cc6bd5ae248f6ee26d395502ecc"));
+
+            byte[] bb = "MD5 test".getBytes();
+            assertTrue(MD5.md5(bb).equals("cc60dda980ccbf65540520703d91b27c"));
+
+            assertTrue(MD5.md5(bb, 4).equals("9f5fb83531d4ceeb734e06cb6a529972"));
+        } catch (Exception e) {
+            fail();
+        }
     }
-  }
 
 }

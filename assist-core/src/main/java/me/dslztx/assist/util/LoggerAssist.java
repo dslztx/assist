969405da1f4ed6b77728f13d450abd741a456e38
dslztx <dslztx@gmail.com>
2018-12-07 17:07:34 +0800

    add LoggerAssist, use slf4j-log4j log4j

---
+++ assist-core/src/main/java/me/dslztx/assist/util/LoggerAssist.java
@@ -0,0 +1,22 @@
+package me.dslztx.assist.util;
+
+import java.io.OutputStreamWriter;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.PatternLayout;
+
+public class LoggerAssist {
+
+    public static void openLogAllConsole() {
+        org.apache.log4j.Logger root = org.apache.log4j.Logger.getRootLogger();
+
+        root.setLevel(Level.ALL);
+
+        ConsoleAppender consoleAppender = new ConsoleAppender();
+        consoleAppender.setWriter(new OutputStreamWriter(System.out));
+        consoleAppender.setLayout(new PatternLayout("%-5p [%t]: %m%n"));
+
+        root.addAppender(consoleAppender);
+    }
+}

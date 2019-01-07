8dcb03ec2e7d31dd628c437bcf961b03420d2aad
dslztx <dslztx@gmail.com>
2019-01-07 22:48:06 +0800

    LIS

--- assist-algorithm/src/main/java/me/dslztx/assist/algorithm/dp/lis/LIS.java
+++ assist-algorithm/src/main/java/me/dslztx/assist/algorithm/dp/lis/LIS.java
@@ -1,65 +1,104 @@
 package me.dslztx.assist.algorithm.dp.lis;
 
+import me.dslztx.assist.util.ArrayAssist;
+
 public class LIS {
 
     int[] sequence;
 
+    int length;
+
     int[] tail;
 
+    int lenOfLIS;
+
+    int[] dp;
+
     public LIS(int[] sequence) {
+        if (ArrayAssist.isEmpty(sequence)) {
+            throw new RuntimeException("sequence is empty");
+        }
+
         this.sequence = sequence;
+        this.length = sequence.length;
     }
 
     public int lengthOfLIS() {
-        int[] dp = new int[sequence.length + 1];
+        dp = new int[length + 1];
 
-        dp[1] = sequence[0];
+        tail = new int[length];
 
         int len = 1;
+        dp[len] = sequence[0];
 
-        for (int pos = 1; pos < sequence.length; pos++) {
-            if (dp[len] < sequence[pos]) {
-                dp[++len] = sequence[pos];
-            } else {
-                int left = 1, right = len, mid;
-                while (left <= right) {
-                    mid = (left + right) >> 1;
+        for (int index = 1; index < length; index++) {
 
-                    if (dp[mid] < sequence[pos]) {
-                        left = mid + 1;
-                    } else {
-                        right = mid - 1;
+            int left = 1, right = len, mid;
+            while (left <= right) {
+                mid = (left + right) >> 1;
+
+                if (dp[mid] < sequence[index]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            len = left;
+            dp[len] = sequence[index];
+            tail[index] = len;
+        }
+
+        lenOfLIS = len;
+
+        return lenOfLIS;
+    }
+
+    public int numberOfLIS() {
+        if (tail == null) {
+            lengthOfLIS();
+        }
+
+        int[] number = new int[length];
+
+        for (int index = 0; index < length; index++) {
+            int num = 0;
+            for (int preIndex = 0; preIndex < index; preIndex++) {
+                if (sequence[preIndex] < sequence[index]) {
+                    if (tail[preIndex] + 1 == tail[index]) {
+                        num += number[preIndex];
+                    }
+                } else if (sequence[preIndex] == sequence[index]) {
+                    if (tail[preIndex] == tail[index]) {
+                        number[preIndex] = 0;
                     }
                 }
+            }
 
-                dp[left] = sequence[pos];
+            if (num == 0) {
+                num = 1;
+            }
+
+            number[index] = num;
+        }
+
+        int sum = 0;
+
+        for (int index = 0; index < length; index++) {
+            if (tail[index] == lenOfLIS) {
+                sum += number[index];
             }
         }
 
-        return len;
+        return sum;
     }
 
-//    public int solutionsOfLIS() {
-//        if (tail == null) {
-//            int len = lengthOfLIS();
-//        }
-//
-//        int[] number = new int[sequence.length];
-//
-//        for (int i = 0; i < sequence.length; i++) {
-//            int num = 0;
-//            for (int j = 0; j < i; j++) {
-//                if (sequence[j] < sequence[i]) {
-//                    if (tail[j] + 1 == tail[i]) {
-//                        num += number[j];
-//                    }
-//                }
-//            }
-//            if (num == 0) {
-//                num = 1;
-//            }
-//
-//            number[i] = num;
-//        }
-//    }
+    public int[] obtainOneSolutionOfLIS() {
+        int[] oneSolution = new int[lenOfLIS];
+        for (int len = lenOfLIS; len >= 1; len--) {
+            oneSolution[len - 1] = dp[len];
+        }
+
+        return oneSolution;
+    }
 }

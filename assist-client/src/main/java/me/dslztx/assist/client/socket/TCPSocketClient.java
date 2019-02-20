2b556f47de2c8c1abfcd0bb7a6b7be3d42f8604f
dslztx <dslztx@gmail.com>
2019-02-20 16:16:58 +0800

    2.0.17 release

--- assist-client/src/main/java/me/dslztx/assist/client/socket/TCPSocketClient.java
+++ assist-client/src/main/java/me/dslztx/assist/client/socket/TCPSocketClient.java
@@ -1,6 +1,7 @@
 package me.dslztx.assist.client.socket;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class TCPSocketClient {
@@ -16,6 +17,35 @@ public class TCPSocketClient {
 
         // 毫秒
         socket.setSoTimeout(timeout);
+    }
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Socket socket = null;
+        try {
+            System.out.println("create " + System.currentTimeMillis());
+            socket = new Socket("127.0.0.1", 10020);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        OutputStream out = socket.getOutputStream();
+
+        for (int i = 0; i < 100; i++) {
+            out.write("hello world".getBytes());
+            out.flush();
+        }
+
+        Thread.sleep(10000);
+
+        System.out.println("terminate " + System.currentTimeMillis());
+
+        socket.shutdownOutput();
+
+        Thread.sleep(10000);
+
+        socket.close();
+
+        Thread.sleep(100000);
     }
 
     public Socket getSocket() {

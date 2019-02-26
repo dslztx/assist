56efd99725d2e2ac9e4a970aae0a0f86923efded
dslztx <dslztx@gmail.com>
2019-02-26 17:06:16 +0800

    KafkaConsumer的配置引用ConsumerConfig

--- assist-client/src/main/java/me/dslztx/assist/client/kafka/KafkaConsumerFactory.java
+++ assist-client/src/main/java/me/dslztx/assist/client/kafka/KafkaConsumerFactory.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -32,12 +33,12 @@ public class KafkaConsumerFactory {
 
             Map<String, Object> props = new HashMap<String, Object>();
 
-            props.put("bootstrap.servers", servers);
-            props.put("group.id", "defaultGroup");
-            props.put("enable.auto.commit", "true");
-            props.put("auto.commit.interval.ms", "1000");
-            props.put("session.timeout.ms", "30000");
-            props.put("fetch.max.bytes", 100 * 1024 * 1024);
+            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+            props.put(ConsumerConfig.GROUP_ID_CONFIG, "defaultGroup");
+            props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+            props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+            props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+            props.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 100 * 1024 * 1024);
 
             return new KafkaConsumer<String, byte[]>(props, new StringDeserializer(), new ByteArrayDeserializer());
         } catch (Exception e) {

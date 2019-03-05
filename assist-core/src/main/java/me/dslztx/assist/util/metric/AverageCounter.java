34cecd51f42098d3b1b9994e9b15d5ce179836e1
dslztx <dslztx@gmail.com>
2019-03-05 15:00:43 +0800

    add getter

--- assist-core/src/main/java/me/dslztx/assist/util/metric/AverageCounter.java
+++ assist-core/src/main/java/me/dslztx/assist/util/metric/AverageCounter.java
@@ -8,6 +8,14 @@ public class AverageCounter {
 
     AtomicLong timeCost = new AtomicLong(0);
 
+    public AtomicLong getCnt() {
+        return cnt;
+    }
+
+    public AtomicLong getTimeCost() {
+        return timeCost;
+    }
+
     /**
      * 这里不需要考虑打印时，只执行了部分统计语句，因为在打印时，需要获取“写”锁，方法执行时持有“读”锁，故必然会等待所有语句执行完成的，最终能够获得“最终统计准确性”
      */

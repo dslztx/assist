3218487004b5558eb3f88988fc3d0c7c8bb3ba72
dslztx <dslztx@gmail.com>
2019-04-08 11:07:56 +0800

    MachineAssist重命名为EnvironmentAssist

---
+++ assist-core/src/main/java/me/dslztx/assist/util/EnvironmentAssist.java
@@ -0,0 +1,65 @@
+package me.dslztx.assist.util;
+
+import java.net.InetAddress;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EnvironmentAssist {
+
+    private static final Logger logger = LoggerFactory.getLogger(EnvironmentAssist.class);
+
+    private static volatile boolean init = false;
+
+    private static String machineName = null;
+    private static String shortMachineName = null;
+
+    public static String obtainShortMachineName() {
+        if (!init) {
+            init();
+        }
+
+        return shortMachineName;
+    }
+
+    public static String obtainLongMachineName() {
+        if (!init) {
+            init();
+        }
+
+        return machineName;
+    }
+
+    private static void init() {
+        if (!init) {
+            synchronized (EnvironmentAssist.class) {
+                if (!init) {
+                    try {
+                        machineName = (InetAddress.getLocalHost()).getHostName();
+
+                        int index = machineName.indexOf(".");
+                        if (index != -1) {
+                            shortMachineName = machineName.substring(0, index);
+                        } else {
+                            shortMachineName = machineName;
+                        }
+                        init = true;
+                    } catch (Exception e) {
+                        logger.error("", e);
+
+                        machineName = null;
+                        shortMachineName = null;
+
+                        init = false;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(obtainLongMachineName());
+        System.out.println(obtainShortMachineName());
+    }
+
+}

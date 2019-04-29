e7efd2e2c7aef37e37bd11d5f998016d916c2e01
dslztx <dslztx@gmail.com>
2019-04-29 14:24:42 +0800

    log4j2配置文件更新，测试Log类更新

--- assist-log/src/main/java/me/dslztx/assist/Log.java
+++ assist-log/src/main/java/me/dslztx/assist/Log.java
@@ -2,6 +2,7 @@ package me.dslztx.assist;
 
 import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.slf4j.LoggerFactory;
 
 public class Log {
@@ -12,11 +13,59 @@ public class Log {
 
     private static final org.slf4j.Logger slf4jLogger = LoggerFactory.getLogger(Log.class);
 
+    private static final org.apache.logging.log4j.Logger log4j2Logger = LogManager.getLogger(Log.class);
+
     public static void main(String[] args) {
-        logger.error("log4j logging");
+        info();
+        warn();
+        error();
+        debug();
+    }
 
-        jclLogger.error("jcl logging");
+    private static void info() {
+        logger.info("log4j info");
 
-        slf4jLogger.error("slf4j logging");
+        jclLogger.info("jcl info");
+
+        slf4jLogger.info("slf4j info");
+
+        log4j2Logger.info("log4j2 info");
+    }
+
+    private static void warn() {
+        logger.warn("log4j warn");
+
+        jclLogger.warn("jcl warn");
+
+        slf4jLogger.warn("slf4j warn");
+
+        log4j2Logger.warn("log4j2 warn");
+    }
+
+    private static void error() {
+        logger.error("log4j error");
+        logger.fatal("log4j fatal");
+
+        jclLogger.error("jcl error");
+        jclLogger.fatal("jcl fatal");
+
+        slf4jLogger.error("slf4j error");
+
+        log4j2Logger.error("log4j2 error");
+        log4j2Logger.fatal("log4j2 fatal");
+    }
+
+    private static void debug() {
+        logger.debug("log4j debug");
+        logger.trace("log4j trace");
+
+        jclLogger.debug("jcl debug");
+        jclLogger.trace("jcl trace");
+
+        slf4jLogger.debug("slf4j debug");
+        slf4jLogger.trace("slf4j trace");
+
+        log4j2Logger.debug("log4j2 debug");
+        log4j2Logger.trace("log4j2 trace");
     }
 }

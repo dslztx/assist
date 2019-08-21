c7cba460f517a52753bfa2e7a135eaaa14d38e42
dslztx <dslztx@gmail.com>
2019-08-21 14:14:20 +0800

    add URLAssist

---
+++ assist-core/src/main/java/me/dslztx/assist/util/URLAssist.java
@@ -0,0 +1,137 @@
+package me.dslztx.assist.util;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class URLAssist {
+
+    public static Set<String> cnSubdomains = new HashSet<String>();
+    private static Pattern ipPattern = Pattern.compile("(\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})(?:\\:\\d+)?");
+    private static Pattern domainPattern =
+        Pattern.compile("\\w{1,50}(?:-\\w{1,50})*(?:\\.\\w{1,50}(?:-\\w{1,50})*){1,}");
+
+    static {
+        cnSubdomains.add("ac");
+        cnSubdomains.add("com");
+        cnSubdomains.add("edu");
+        cnSubdomains.add("gov");
+        cnSubdomains.add("net");
+        cnSubdomains.add("org");
+        cnSubdomains.add("co");
+
+        cnSubdomains.add("TRAVEL".toLowerCase());
+        cnSubdomains.add("TEL".toLowerCase());
+        cnSubdomains.add("MUSEUM".toLowerCase());
+        cnSubdomains.add("MOBI".toLowerCase());
+        cnSubdomains.add("MIL".toLowerCase());
+        cnSubdomains.add("JOBS".toLowerCase());
+        cnSubdomains.add("INT".toLowerCase());
+        cnSubdomains.add("INFO".toLowerCase());
+        cnSubdomains.add("COOP".toLowerCase());
+        cnSubdomains.add("BIZ".toLowerCase());
+        cnSubdomains.add("ASIA".toLowerCase());
+        cnSubdomains.add("AERO".toLowerCase());
+    }
+
+    public static String obtainURLDomain(String url) {
+        String domain = getDomain(url);
+        domain = domain.trim();
+        domain = domain.replaceAll("%20", "");
+        String[] ss = domain.split("\\.");
+        if (ss.length == 2 && ss[1].length() == 2 && cnSubdomains.contains(ss[0])) {
+            return null;
+        }
+        return domain != null && domainPattern.matcher(domain).matches() ? domain : null;
+    }
+
+    public static String getDomain(String url) {
+        if (url == null) {
+            return null;
+        }
+        // 去掉参数
+        if (url.contains("?")) {
+            url = url.substring(0, url.indexOf("?"));
+        }
+        if (url.contains("#")) {
+            url = url.substring(0, url.indexOf("#"));
+        }
+        // 去掉协议
+        url = removeHTTP(url);
+        // 去掉path部分;
+        int idx = url.indexOf("/");
+        if (idx >= 0) {
+            url = url.substring(0, idx);
+        }
+        idx = url.indexOf("\\");
+        if (idx >= 0) {
+            url = url.substring(0, idx);
+        }
+        Matcher m = ipPattern.matcher(url);
+        if (m.matches()) {
+            return m.group(1);
+        }
+        // 去掉端口
+        idx = url.lastIndexOf(":");
+        if (idx >= 0) {
+            url = url.substring(0, idx);
+        }
+        String[] ss = url.split("\\.");
+        // boolean isCnSubDomain = ss.length > 2 && ss[ss.length - 1].length() == 2 && ss[ss.length - 2].length() < 4;
+        boolean isCnSubDomain = false;
+        if (!isCnSubDomain) {
+            isCnSubDomain = ss.length > 2 && cnSubdomains.contains(ss[ss.length - 2]);
+        }
+        if (isCnSubDomain) {
+            return ss[ss.length - 3] + "." + ss[ss.length - 2] + "." + ss[ss.length - 1];
+        } else if (ss.length >= 2) {
+            return ss[ss.length - 2] + "." + ss[ss.length - 1];
+        } else {
+            return url;
+        }
+    }
+
+    public static String removeHTTP(String url) {
+        int start = removeHTTPStart(url);
+        if (start > 0)
+            return url.substring(start);
+        return url;
+    }
+
+    public static int removeHTTPStart(String url) {
+        String lowUrl = url.toLowerCase();
+        int start = 0;
+        if (lowUrl.startsWith("http:")) {
+            start = "http:".length();
+            while (start < lowUrl.length()) {
+                char c = lowUrl.charAt(start);
+                if (c == '/' || c == '\\')
+                    start++;
+                else
+                    break;
+            }
+        }
+        if (lowUrl.startsWith("https:")) {
+            start = "https:".length();
+            while (start < lowUrl.length()) {
+                char c = lowUrl.charAt(start);
+                if (c == '/' || c == '\\')
+                    start++;
+                else
+                    break;
+            }
+        }
+        if (lowUrl.startsWith("ftp:")) {
+            start = "ftp:".length();
+            while (start < lowUrl.length()) {
+                char c = lowUrl.charAt(start);
+                if (c == '/' || c == '\\')
+                    start++;
+                else
+                    break;
+            }
+        }
+        return start;
+    }
+}

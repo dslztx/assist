5ec162c6babe2cf8c8a3bdf2e834e0bcb635a92c
dslztx <dslztx@gmail.com>
2020-01-07 15:25:07 +0800

    增加asssit-text子模块

---
+++ assist-text/src/main/java/me/dslztx/assist/text/TextProcessAssist.java
@@ -0,0 +1,9 @@
+package me.dslztx.assist.text;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TextProcessAssist {
+    public static final Logger logger = LoggerFactory.getLogger(TextProcessAssist.class);
+
+}

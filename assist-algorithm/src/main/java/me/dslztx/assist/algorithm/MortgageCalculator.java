3eb2e86c682cbd385826392ce54e1fdd5bc91796
dslztx <dslztx@gmail.com>
2020-12-14 14:14:43 +0800

    房贷计算

---
+++ assist-algorithm/src/main/java/me/dslztx/assist/algorithm/MortgageCalculator.java
@@ -0,0 +1,62 @@
+package me.dslztx.assist.algorithm;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+/**
+ * 房贷计算，等额本金和等额本息
+ */
+public class MortgageCalculator {
+
+    /**
+     * https://baike.baidu.com/item/%E7%AD%89%E9%A2%9D%E6%9C%AC%E9%87%91
+     */
+    public static BigDecimal benjin(int totalLoan, double monthRatio, int originMonth, int actualMonth) {
+        BigDecimal loan = BigDecimal.valueOf(totalLoan);
+
+        BigDecimal ratio = BigDecimal.valueOf(monthRatio);
+
+        BigDecimal monthRepayment = loan.divide(BigDecimal.valueOf(originMonth), 3, RoundingMode.HALF_UP);
+
+        BigDecimal sum = BigDecimal.valueOf(0);
+
+        for (int i = 1; i <= actualMonth; i++) {
+            sum = sum.add(monthRepayment).add(loan.multiply(ratio));
+
+            loan = loan.subtract(monthRepayment);
+        }
+
+        return sum.add(loan).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    /**
+     * 参考 https://www.cnblogs.com/hanganglin/p/6777838.html
+     */
+    public static BigDecimal benxi(int totalLoan, double monthRatio, int originMonth, int actualMonth) {
+
+        BigDecimal loan = BigDecimal.valueOf(totalLoan);
+
+        BigDecimal ratio = BigDecimal.valueOf(monthRatio);
+
+        BigDecimal factor = BigDecimal.valueOf(Math.pow(1 + ratio.doubleValue(), originMonth));
+
+        BigDecimal monthRepayment = loan.multiply(ratio).multiply(factor).divide(factor.subtract(BigDecimal.valueOf(1)),
+            3, RoundingMode.HALF_UP);
+
+        BigDecimal leftRepayment = loan.multiply(factor);
+
+        BigDecimal sum0 = BigDecimal.valueOf(1);
+        for (int i = 1; i <= actualMonth - 1; i++) {
+            BigDecimal tmp = BigDecimal.valueOf(Math.pow(ratio.doubleValue() + 1, i));
+
+            sum0 = sum0.add(tmp);
+        }
+
+        sum0 = sum0.multiply(monthRepayment);
+
+        leftRepayment = leftRepayment.subtract(sum0);
+
+        return leftRepayment.add(monthRepayment.multiply(BigDecimal.valueOf(actualMonth))).setScale(2,
+            RoundingMode.HALF_UP);
+    }
+}
---
+++ assist-algorithm/src/test/java/me/dslztx/assist/algorithm/MortgageCalculatorTest.java
@@ -0,0 +1,42 @@
+package me.dslztx.assist.algorithm;
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MortgageCalculatorTest {
+    private static final Logger logger = LoggerFactory.getLogger(MortgageCalculatorTest.class);
+
+    @Test
+    public void benjin() {
+        try {
+
+            double doubleMonthRatio = 0.0538 / 12;
+
+            BigDecimal r = MortgageCalculator.benjin(2120000, doubleMonthRatio, 240, 240);
+            Assert.assertTrue(r.compareTo(BigDecimal.valueOf(3265312.38)) == 0);
+
+        } catch (Exception e) {
+            logger.error("", e);
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void benxi() {
+        try {
+
+            double doubleMonthRatio = 0.0538 / 12;
+
+            BigDecimal r = MortgageCalculator.benxi(2120000, doubleMonthRatio, 240, 240);
+            Assert.assertTrue(r.compareTo(BigDecimal.valueOf(3465576.14)) == 0);
+
+        } catch (Exception e) {
+            logger.error("", e);
+            Assert.fail();
+        }
+    }
+}

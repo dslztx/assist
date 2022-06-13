235987c282099e1e125a3e769472e2dfe59d1e5a
dslztx <dslztx@gmail.com>
2022-06-13 17:53:23 +0800

    增加注释

--- assist-core/src/main/java/me/dslztx/assist/util/ExecuteShellAssist.java
+++ assist-core/src/main/java/me/dslztx/assist/util/ExecuteShellAssist.java
@@ -8,14 +8,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.*;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import lombok.extern.slf4j.Slf4j;
 import me.dslztx.assist.util.concurrent.NamedThreadFactory;
 
+@Slf4j
 public class ExecuteShellAssist {
-
-    private static final Logger logger = LoggerFactory.getLogger(ExecuteShellAssist.class);
 
     private static ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 20, 60, TimeUnit.SECONDS,
         new SynchronousQueue<Runnable>(), new NamedThreadFactory("shell-execute", true));
@@ -33,7 +30,7 @@ public class ExecuteShellAssist {
             Process process = new ProcessBuilder(commandWithArguments).start();
             process.waitFor();
         } catch (Exception e) {
-            logger.error("", e);
+            log.error("", e);
             throw new RuntimeException(e);
         }
     }
@@ -50,7 +47,7 @@ public class ExecuteShellAssist {
 
             new ProcessBuilder(commandWithArguments).start();
         } catch (Exception e) {
-            logger.error("", e);
+            log.error("", e);
             throw new RuntimeException(e);
         }
     }
@@ -70,14 +67,16 @@ public class ExecuteShellAssist {
             Future<List<String>> resultFuture = executor.submit(new SubprocessReadAsync(process));
 
             try {
-                return resultFuture.get(3, TimeUnit.MINUTES);
+                return resultFuture.get(1, TimeUnit.MINUTES);
             } catch (Exception e) {
+                log.error("execute shell exception", e);
+
                 process.destroyForcibly();
 
                 return new ArrayList<>();
             }
         } catch (Exception e) {
-            logger.error("", e);
+            log.error("", e);
             throw new RuntimeException(e);
         }
     }

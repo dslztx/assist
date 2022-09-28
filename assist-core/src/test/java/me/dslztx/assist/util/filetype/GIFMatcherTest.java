3e4662dc23ac32d9f260cdc06a5f32924ec0c1ff
dslztx <dslztx@gmail.com>
2022-09-28 11:15:49 +0800

    引入mockito依赖

--- assist-core/src/test/java/me/dslztx/assist/util/filetype/GIFMatcherTest.java
+++ assist-core/src/test/java/me/dslztx/assist/util/filetype/GIFMatcherTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,6 +22,19 @@ public class GIFMatcherTest {
             byte[] data = IOUtils.toByteArray(ClassPathResourceAssist.locateInputStream("images/1.gif"));
 
             Assert.assertTrue(giFMatcher.match(data));
+        } catch (Exception e) {
+            logger.error("", e);
+            fail();
+        }
+    }
+
+    @Test
+    public void match2() {
+        try {
+            GIFMatcher gifMatcher = Mockito.mock(GIFMatcher.class);
+
+            Mockito.when(gifMatcher.match(Mockito.any())).thenReturn(true);
+            Assert.assertTrue(gifMatcher.match(null));
         } catch (Exception e) {
             logger.error("", e);
             fail();

cbb6a3a3d04fd2a6d4273cf616da1b283ee00e1b
dslztx <dslztx@gmail.com>
2023-05-04 11:23:20 +0800

    参数项通过Config类指定

--- assist-client/src/main/java/me/dslztx/assist/client/kafka/KafkaConsumerFactory.java
+++ assist-client/src/main/java/me/dslztx/assist/client/kafka/KafkaConsumerFactory.java
@@ -63,16 +63,16 @@ public class KafkaConsumerFactory {
 
             Map<String, Object> props = new HashMap<String, Object>();
 
-            props.put("bootstrap.servers", servers);
-            props.put("group.id", groupId);
-            props.put("max.partition.fetch.bytes", 20971520);
+            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+            props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+            props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, 20971520);
             // 这个参数十分重要，影响消费行为：https://blog.csdn.net/lishuangzhe7047/article/details/74530417
-            props.put("auto.offset.reset", "earliest");
-            props.put("max.poll.records", "3000");
-            props.put("enable.auto.commit", "true");
-            props.put("auto.commit.interval.ms", "1000");
-            props.put("session.timeout.ms", "30000");
-            props.put("fetch.max.bytes", 100 * 1024 * 1024);
+            props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "3000");
+            props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+            props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+            props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+            props.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 100 * 1024 * 1024);
 
             return new KafkaConsumer<String, byte[]>(props, new StringDeserializer(), new ByteArrayDeserializer());
         } catch (Exception e) {

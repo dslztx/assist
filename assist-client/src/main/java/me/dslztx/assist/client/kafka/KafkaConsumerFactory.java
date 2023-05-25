8dbb20cf261f307b1fea28cc6e46dd9acc031d3c
dslztx <dslztx@gmail.com>
2023-05-25 09:45:21 +0800

    kafka消费者也支持用户名和密码

--- assist-client/src/main/java/me/dslztx/assist/client/kafka/KafkaConsumerFactory.java
+++ assist-client/src/main/java/me/dslztx/assist/client/kafka/KafkaConsumerFactory.java
@@ -35,10 +35,27 @@ public class KafkaConsumerFactory {
 
             props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
             props.put(ConsumerConfig.GROUP_ID_CONFIG, "defaultGroup");
+            props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, 20971520);
+            // 这个参数十分重要，影响消费行为：https://blog.csdn.net/lishuangzhe7047/article/details/74530417
+            props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "3000");
             props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
             props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
             props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
             props.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 100 * 1024 * 1024);
+
+            String username = configuration.getString("kafka.servers.username");
+            String password = configuration.getString("kafka.servers.password");
+
+            if (StringAssist.isNotBlank(username) && StringAssist.isNotBlank(password)) {
+                props.put("security.protocol", "SASL_PLAINTEXT");
+                props.put("sasl.mechanism", "PLAIN");
+
+                String auth = StringAssist.format("org.apache.kafka.common.security.plain"
+                    + ".PlainLoginModule required username=\"{}\" password=\"{}\";", username, password);
+
+                props.put("sasl.jaas.config", auth);
+            }
 
             return new KafkaConsumer<String, byte[]>(props, new StringDeserializer(), new ByteArrayDeserializer());
         } catch (Exception e) {
@@ -73,6 +90,19 @@ public class KafkaConsumerFactory {
             props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
             props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
             props.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 100 * 1024 * 1024);
+
+            String username = configuration.getString("kafka.servers.username");
+            String password = configuration.getString("kafka.servers.password");
+
+            if (StringAssist.isNotBlank(username) && StringAssist.isNotBlank(password)) {
+                props.put("security.protocol", "SASL_PLAINTEXT");
+                props.put("sasl.mechanism", "PLAIN");
+
+                String auth = StringAssist.format("org.apache.kafka.common.security.plain"
+                    + ".PlainLoginModule required username=\"{}\" password=\"{}\";", username, password);
+
+                props.put("sasl.jaas.config", auth);
+            }
 
             return new KafkaConsumer<String, byte[]>(props, new StringDeserializer(), new ByteArrayDeserializer());
         } catch (Exception e) {

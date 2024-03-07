1e7571a062d9b824b7add386bf8224976bed9c8c
dslztx <dslztx@gmail.com>
2024-03-07 17:11:34 +0800

    TextAssist不再需要，相关的逻辑移到LanguageRecognizeAssist

--- assist-nlp/src/main/java/me/dslztx/assist/nlp/LanguageRecognizeAssist.java
+++ assist-nlp/src/main/java/me/dslztx/assist/nlp/LanguageRecognizeAssist.java
@@ -1,7 +1,13 @@
 package me.dslztx.assist.nlp;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
+import me.dslztx.assist.util.CollectionAssist;
+import me.dslztx.assist.util.ObjectAssist;
 import me.dslztx.assist.util.StringAssist;
 
 /**
@@ -12,12 +18,30 @@ public class LanguageRecognizeAssist {
     /**
      * 中文汉字（不包括中文标点符号）UnicodeRange下界值，其对应十进制值为19968
      */
-    private static int CHINESE_CHAR_UNICODE_RANGE_LOWER_BOUND = 0x4e00;
-
+    private static final int CHINESE_CHAR_UNICODE_RANGE_LOWER_BOUND = 0x4e00;
     /**
      * 中文汉字（不包括中文标点符号）UnicodeRange上界值，其对应十进制值为40869
      */
-    private static int CHINESE_CHAR_UNICODE_RANGE_UPPER_BOUND = 0x9fa5;
+    private static final int CHINESE_CHAR_UNICODE_RANGE_UPPER_BOUND = 0x9fa5;
+
+    private static final LanguageRecognizeAssist.Language[] lls =
+        new LanguageRecognizeAssist.Language[] {LanguageRecognizeAssist.Language.CHINESE,
+            LanguageRecognizeAssist.Language.KOREA, LanguageRecognizeAssist.Language.JAPAN,
+            LanguageRecognizeAssist.Language.GREEK, LanguageRecognizeAssist.Language.LATIN,
+            LanguageRecognizeAssist.Language.ARABIC, LanguageRecognizeAssist.Language.CYRILLIC};
+
+    private static final Map<Character.UnicodeScript, List<LanguageRecognizeAssist.Language>> map = new HashMap<>();
+
+    static {
+        map.put(Character.UnicodeScript.HAN,
+            Arrays.asList(LanguageRecognizeAssist.Language.CHINESE, LanguageRecognizeAssist.Language.JAPAN));
+        map.put(Character.UnicodeScript.HANGUL, Arrays.asList(LanguageRecognizeAssist.Language.KOREA));
+        map.put(Character.UnicodeScript.HIRAGANA, Arrays.asList(LanguageRecognizeAssist.Language.JAPAN));
+        map.put(Character.UnicodeScript.GREEK, Arrays.asList(LanguageRecognizeAssist.Language.GREEK));
+        map.put(Character.UnicodeScript.LATIN, Arrays.asList(LanguageRecognizeAssist.Language.LATIN));
+        map.put(Character.UnicodeScript.ARABIC, Arrays.asList(LanguageRecognizeAssist.Language.ARABIC));
+        map.put(Character.UnicodeScript.CYRILLIC, Arrays.asList(LanguageRecognizeAssist.Language.CYRILLIC));
+    }
 
     /**
      * 根据UnicodeRange识别中文汉字（不包括中文标点符号）
@@ -138,5 +162,96 @@ public class LanguageRecognizeAssist {
         }
 
         return false;
+    }
+
+    public static LanguageRecognizeAssist.Language guessLanguage(String text) {
+        if (StringAssist.isBlank(text)) {
+            return LanguageRecognizeAssist.Language.UNKNOWN;
+        }
+
+        Map<LanguageRecognizeAssist.Language, Integer> cnt = new HashMap<>();
+        char c;
+        Character.UnicodeScript cu;
+        List<LanguageRecognizeAssist.Language> languages;
+
+        for (int index = 0; index < text.length(); index++) {
+            c = text.charAt(index);
+
+            cu = Character.UnicodeScript.of((int)c);
+
+            if (ObjectAssist.isNotNull(cu)) {
+                languages = map.get(cu);
+
+                if (CollectionAssist.isNotEmpty(languages)) {
+                    for (LanguageRecognizeAssist.Language language : languages) {
+                        if (ObjectAssist.isNull(cnt.get(language))) {
+                            cnt.put(language, 0);
+                        }
+
+                        cnt.put(language, cnt.get(language) + 1);
+                    }
+                }
+
+            }
+        }
+
+        int maxv = -1;
+        LanguageRecognizeAssist.Language selected = null;
+
+        for (LanguageRecognizeAssist.Language language : lls) {
+            if (ObjectAssist.isNotNull(cnt.get(language)) && cnt.get(language) > maxv) {
+                maxv = cnt.get(language);
+                selected = language;
+            }
+        }
+
+        if (ObjectAssist.isNull(selected)) {
+            return LanguageRecognizeAssist.Language.UNKNOWN;
+        } else {
+            if (maxv * 10 > text.length() * 3) {
+                return selected;
+            } else {
+                return LanguageRecognizeAssist.Language.UNKNOWN;
+            }
+        }
+    }
+
+    public static enum Language {
+        /**
+         * 汉语
+         */
+        CHINESE,
+
+        /**
+         * 朝鲜语
+         */
+        KOREA,
+
+        /**
+         * 日本语
+         */
+        JAPAN,
+
+        /**
+         * 希腊语
+         */
+        GREEK,
+
+        /**
+         * 拉丁字母语系，可能是英语、法语、德语、西班牙语、意大利语等
+         */
+        LATIN,
+
+        /**
+         * 阿拉伯字母语系，可能是阿拉伯语、哈萨克语、吉尔吉斯语、维吾尔语等
+         */
+        ARABIC,
+
+        /**
+         * 斯拉夫字母语系，可能是俄语、乌克兰语、卢森尼亚语、白俄罗斯语、保加利亚语、塞尔维亚语、马其顿语等
+         */
+        CYRILLIC,
+
+        UNKNOWN;
     }
 }
--- assist-nlp/src/test/java/me/dslztx/assist/nlp/LanguageRecognizeAssistTest.java
+++ assist-nlp/src/test/java/me/dslztx/assist/nlp/LanguageRecognizeAssistTest.java
@@ -126,4 +126,57 @@ public class LanguageRecognizeAssistTest {
             Assert.fail();
         }
     }
+
+    @Test
+    public void guessLanguageTest() {
+        try {
+            String text0 = "你好啊，这是一段测试文字，hello world";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text0) == LanguageRecognizeAssist.Language.CHINESE);
+
+            String text1 = "나는 가난한 탁발승이오. 내가 가진 거라고는 물레와 교도소에서 쓰던 밥그릇과 염소 젖 한 깡통, 허름한 요포 여섯장, 수건 그리고 대단치도 않은 평판 이것 "
+                + "뿐이오. hello world 这是朝鲜语";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text1) == LanguageRecognizeAssist.Language.KOREA);
+
+            String text2 = "うみは ひろいな\n" + "おおきいな\n" + "つきは のぼるし\n" + "ひがしずむ\n" + "\n" + "うみは おおなみ\n" + "あおいなみ\n"
+                + "ゆれて どこまで\n" + "つづくやら\n" + "\n" + "うみに おふねを\n" + "うかばせて\n" + "いって みたいな\n" + "よそのくに\n" + "\n"
+                + "うみは ひろいな\n" + "おおきいな\n" + "つきは のぼるし\n" + "ひがしずむ  hello world 这是日语";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text2) == LanguageRecognizeAssist.Language.JAPAN);
+
+            String text3 =
+                "Почти треть россиян (30%) придерживаются однозначного мнения, что отец может не хуже матери "
+                    + "ухаживать hello world 这是俄语";
+            Assert
+                .assertTrue(LanguageRecognizeAssist.guessLanguage(text3) == LanguageRecognizeAssist.Language.CYRILLIC);
+
+            String text4 = "I am a 12 years old smart and handsome boy..I am proud of myself.Because I can get "
+                + "perfect scores 这是英语";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text4) == LanguageRecognizeAssist.Language.LATIN);
+
+            String text5 =
+                "Debout les damnés de la terre Debout les forçats de la faim La raison tonne en son cratère " + "这是法语";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text5) == LanguageRecognizeAssist.Language.LATIN);
+
+            String text6 = "Wie lange wollen Sie hier bleiben, Wieviel kostet das, Darf ich kurz dich stoeren 这是德语";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text6) == LanguageRecognizeAssist.Language.LATIN);
+
+            String text7 = "Fidanzamento e matrimonio, Gravidanza,parto ,Separazione,divorzio, 这是意大利语";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text7) == LanguageRecognizeAssist.Language.LATIN);
+
+            String text8 = "مرحبا\n" + "كيف حالك؟\n" + "这是阿拉伯语.انا بخير, شكرا";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text8) == LanguageRecognizeAssist.Language.ARABIC);
+
+            String text9 = "قارشى ئالىمىز\n" + "ئەسسالامۇ ئەلەيكۇم!\n" + "ياخشىمۇسىز؟这是维吾尔语";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text9) == LanguageRecognizeAssist.Language.ARABIC);
+
+            String text10 = "Αυτό το απόσπασμα， η αγάπη μερικές φορές για πάντα， μερικές φορές αξέχαστη. Αυτό είναι "
+                + "το όνομα και του χαρακτήρα του， Qin Shuyi 这是希腊语";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text10) == LanguageRecognizeAssist.Language.GREEK);
+
+            String text11 = "階段などの一きざみ。転じて、事件の一くぎり、地位・技能の一段階など";
+            Assert.assertTrue(LanguageRecognizeAssist.guessLanguage(text11) == LanguageRecognizeAssist.Language.JAPAN);
+        } catch (Exception e) {
+            log.error("", e);
+            Assert.fail();
+        }
+    }
 }

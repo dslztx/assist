6ae35740d5763636d9a12e3669c0aec4dc07fb11
dslztx <dslztx@gmail.com>
2024-03-29 00:29:40 +0800

    tmp2

--- assist-core/src/main/java/me/dslztx/assist/util/JSONAssist.java
+++ assist-core/src/main/java/me/dslztx/assist/util/JSONAssist.java
@@ -15,40 +15,78 @@ public class JSONAssist {
         return objectMapper.writeValueAsString(obj);
     }
 
-    public static <T> T parseObjectForbidMapOrCollectionOrArray(String s, Class<T> clz) throws JsonProcessingException {
+    public static <T> T parseObjectForbidMapOrCollectionOrArray(String jsonString, Class<T> clz)
+        throws JsonProcessingException {
+
+        if (StringAssist.isBlank(jsonString)) {
+            throw new RuntimeException("jsonString is blank");
+        }
+
+        if (Objects.isNull(clz)) {
+            throw new RuntimeException("object class is null");
+        }
+
         if (Map.class.isAssignableFrom(clz) || List.class.isAssignableFrom(clz) || clz.isArray()) {
             throw new UnsupportedOperationException(
                 "can not support map/collection/array object deserialize, use " + "specific method instead");
         }
 
-        return objectMapper.readValue(s, clz);
+        return objectMapper.readValue(jsonString, clz);
     }
 
-    public static <K, V> Map<K, V> parseMap(String s, Class<K> kClass, Class<V> vClass) throws JsonProcessingException {
+    public static <K, V> Map<K, V> parseMap(String jsonString, Class<K> keyClz, Class<V> valueClz)
+        throws JsonProcessingException {
+
+        if (StringAssist.isBlank(jsonString)) {
+            throw new RuntimeException("jsonString is blank");
+        }
+
+        if (Objects.isNull(keyClz)) {
+            throw new RuntimeException("map key class is null");
+        }
+
+        if (Objects.isNull(valueClz)) {
+            throw new RuntimeException("map value class is null");
+        }
 
         ObjectMapper localMapper = new ObjectMapper();
 
-        JavaType javaType = localMapper.getTypeFactory().constructMapType(HashMap.class, kClass, vClass);
+        JavaType javaType = localMapper.getTypeFactory().constructMapType(HashMap.class, keyClz, valueClz);
 
-        return localMapper.readValue(s, javaType);
+        return localMapper.readValue(jsonString, javaType);
     }
 
-    public static <K> List<K> parseList(String s, Class<K> kClass) throws JsonProcessingException {
+    public static <K> List<K> parseList(String jsonString, Class<K> elementClz) throws JsonProcessingException {
+
+        if (StringAssist.isBlank(jsonString)) {
+            throw new RuntimeException("jsonString is blank");
+        }
+
+        if (Objects.isNull(elementClz)) {
+            throw new RuntimeException("element class is null");
+        }
 
         ObjectMapper localMapper = new ObjectMapper();
 
-        JavaType javaType = localMapper.getTypeFactory().constructCollectionType(ArrayList.class, kClass);
+        JavaType javaType = localMapper.getTypeFactory().constructCollectionType(ArrayList.class, elementClz);
 
-        return localMapper.readValue(s, javaType);
+        return localMapper.readValue(jsonString, javaType);
     }
 
-    public static <K> K[] parseArray(String s, Class<K> kClass) throws JsonProcessingException {
+    public static <K> K[] parseArray(String jsonString, Class<K> elementClz) throws JsonProcessingException {
+        if (StringAssist.isBlank(jsonString)) {
+            throw new RuntimeException("jsonString is blank");
+        }
 
-        List<K> tt = parseList(s, kClass);
+        if (Objects.isNull(elementClz)) {
+            throw new RuntimeException("element class is null");
+        }
 
-        K[] array = (K[])Array.newInstance(kClass, tt.size());
+        List<K> listResult = parseList(jsonString, elementClz);
 
-        return (K[])tt.toArray(array);
+        K[] arrayResult = (K[])Array.newInstance(elementClz, listResult.size());
+
+        return (K[])listResult.toArray(arrayResult);
 
     }
 
